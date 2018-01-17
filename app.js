7dffdb17a9c7af86345673da1a73b3573c3c57b9
GuilhermeRodrigues <guilherme17_rodrigues@hotmail.com>
2018-01-16 23:28:36 -0200

    Atualizando a relação de participantes
    Atualizando a relação de participantes da conversa.

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ io.on('connection', function(socket){
     });
 
     socket.on('msgParaServidor', function(data){
+
+        /* dialogo */
         socket.emit(
             'msgParaCliente', 
             {apelido : data.apelido, mensagem : data.mensagem}
@@ -28,6 +30,20 @@ io.on('connection', function(socket){
             'msgParaCliente', 
             {apelido : data.apelido, mensagem : data.mensagem}
         );
+
+        /* participantes */
+        if(parseInt(data.apelido_atualizado_nos_clientes) == 0){
+            socket.emit(
+                'participantesParaCliente', 
+                {apelido : data.apelido}
+            );
+
+            socket.broadcast.emit(
+                'participantesParaCliente', 
+                {apelido : data.apelido}
+            );
+        }
+
     });
 });
 
